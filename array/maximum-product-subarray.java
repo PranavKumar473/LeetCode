d2b9585570c6d91d42bdcea180ebbf370e2c8b2f
PranavKumar473 <143784100+PranavKumar473@users.noreply.github.com>
2026-08-12 04:04:15 +0530

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-product-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] arr) {
+        int product = 1;
+        int answer = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+        if(product >= 0){
+        product = product*arr[i];
+        }
+        else{
+            product = arr[i];
+        }
+        answer = Math.max(answer,product);
+        }
+        return answer;
+    }
+}
