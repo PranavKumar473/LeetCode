2a37e10f5bc972a45a8598323c03ffb59ec3ec63
PranavKumar473 <143784100+PranavKumar473@users.noreply.github.com>
2026-08-15 13:33:20 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- array/merge-sorted-array.java
+++ array/merge-sorted-array.java
@@ -1,29 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-      
-        int p = 0;
-        int q = 0;
+        int p1 = m - 1;        // Last element in valid nums1
+        int p2 = n - 1;        // Last element in nums2
+        int p = m + n - 1;     // Placement pointer at the back of nums1
 
-        while(p<(m+n-1) && q<n){
-            if(nums1[p]<nums2[q]){
-                p++;
+        // Compare from the back while both arrays have elements left
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            else{
-                if(nums1[p]!=0){
-                    nums1[p+1] = nums1[p];
-                    nums1[p]  =  nums2[q];
-                    q++;
-                    p++;
-                }
-                else{
-                    nums1[p]  =  nums2[q];
-                    q++;
-                    p++;
-                }
-            }    
+            p--;
         }
-        for(int i=0;i<nums1.length;i++){
-        System.out.print(nums1[i]+",");
+
+        // If nums2 still has leftover elements, copy them over
+        // (If nums1 has leftovers, they are already in their correct sorted spots!)
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
         }
     }
 }
